eaa05c9e5de9f7b43989a6d3fe92bf8ab3bf88e5
Omar Ibrahim <omaribrahim@Omars-MacBook-Air-2.local>
2025-01-29 17:12:15 +0200

    GET api articles with comment count

--- app.js
+++ app.js
@@ -2,24 +2,39 @@ const express = require("express");
 const app = express();
 const getEndpoints = require("./controller/getEndpoint");
 const { getAlltopics } = require("./controller/topics.controller");
-const { getArticleId } = require("./controller/articleId.controller");
+const {
+  getArticleId,
+  getArticles,
+} = require("./controller/article.controller");
 
 app.get("/api", getEndpoints);
 app.get("/api/topics", getAlltopics);
 app.get("/api/articles/:article_id", getArticleId);
+app.get("/api/articles", getArticles);
 
 //  error handling midleware
 
+app.all("*", (req, res) => {
+  res.status(404).send({ msg: "Endpoint not found" });
+});
+app.use((err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "Invalid Data Type" });
+  } else {
+    next(err);
+  }
+});
 app.use((err, req, res, next) => {
   if (err.status && err.msg) {
     res.status(err.status).send({ msg: err.msg });
   } else {
-    next();
+    next(err);
   }
 });
 
-app.all("*", (req, res) => {
-  res.status(404).send({ error: "Endpoint not found" });
+app.use((err, req, res) => {
+  console.log(err);
+  res.status(500).send({ msg: "Internal Error" });
 });
 
 module.exports = app;
---
+++ controller/article.controller.js
@@ -0,0 +1,38 @@
+const articles = require("../db/data/test-data/articles");
+const {
+  selectArticleById,
+  selectArticles,
+} = require("../models/articles.model");
+
+const getArticleId = (req, res, next) => {
+  const articleId = req.params.article_id;
+
+  selectArticleById(articleId)
+    .then((article) => {
+      res.status(200).send({ article });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+const getArticles = (req, res, next) => {
+  // const body = req.body;
+  selectArticles()
+    .then((articles) => {
+      // if (articles.length === 0) {
+      //   res.status(200).send([]);
+      // }
+      res.status(200).send({ articles });
+
+      // console.log(articles);
+      // console.log(articles);
+    })
+    .catch((err) => {
+      console.log(err);
+
+      next(err);
+    });
+};
+
+module.exports = { getArticleId, getArticles };
