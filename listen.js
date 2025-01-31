0291dd1d7cfae3d52def9ff424b5d1d2002ab349
Omar Ibrahim <omaribrahim@Omars-MacBook-Air-2.local>
2025-01-31 17:16:34 +0200

    superbase setup

--- listen.js
+++ listen.js
@@ -4,3 +4,6 @@ const server = app.listen(8080, (err) => {
   if (err) console.error("Server is not running :", err.message);
   console.log("Server is running on port 8080");
 });
+
+const { PORT = 9090 } = process.env;
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
