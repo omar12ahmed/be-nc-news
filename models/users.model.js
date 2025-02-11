209337e54c60b383f404053dddea520713a9c145
Omar Ibrahim <omaribrahim@Omars-MacBook-Air-2.local>
2025-02-11 08:43:31 +0200

    .

--- models/users.model.js
+++ models/users.model.js
@@ -2,7 +2,8 @@ const db = require("../db/connection");
 
 const selectUsers = () => {
   return db.query(`SELECT * FROM users `).then(({ rows }) => {
-    return rows;
+    return rows; //
+    //
   });
 };
 
