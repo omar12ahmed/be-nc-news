6e1d4378ea67052fdef13c3af609db223aa774e9
Omar Ibrahim <omaribrahim@Omars-MacBook-Air-2.local>
2025-02-17 16:01:44 +0200

    refactored listen file

--- listen.js
+++ listen.js
@@ -1,9 +1,4 @@
 const app = require("./app");
 
-const server = app.listen(8080, (err) => {
-  if (err) console.error("Server is not running :", err.message);
-  console.log("Server is running on port 8080");
-});
-
 const { PORT = 9090 } = process.env;
 app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
